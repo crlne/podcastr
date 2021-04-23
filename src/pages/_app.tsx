5faaf9081c2487a559a5e8f990cc7d1cfb19e011
crlne <carol.lages@outlook.com>
2021-04-23 14:44:07 -0300

    player context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+import { PlayerContext } from '../context/PlayerContext';
+
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss'
-import { Player } from '../components/Player';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+ 
+  function play(episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
     <div className={styles.appWrapper}>
      <main>
       <Header />
@@ -12,6 +24,7 @@ function MyApp({ Component, pageProps }) {
      </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
   )  
 }
 
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -27,6 +27,7 @@ type Episode = {
 }
   
 export default function Episode({ episode }: EpisodeProps) {
+ 
     return (
         <div className={styles.episodes}>
             <div className={styles.thumbnailContainer}>
@@ -55,10 +56,26 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: { 
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return {
-        path: [],
+        paths,
         fallback: 'blocking'
-    }
+    }       
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
